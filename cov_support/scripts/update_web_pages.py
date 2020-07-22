6527ec2edc3fddc60c20bcd81aa08100f7245771
aineniamh <aine.otoole@ed.ac.uk>
2020-07-22 11:52:52 +0100

    bug fix web page script

--- cov_support/scripts/update_web_pages.py
+++ cov_support/scripts/update_web_pages.py
@@ -36,22 +36,23 @@ def create_lineage_dict(lineage_csv):
                     lineage_dict[parents].append((lineage, l))
     return lineage_dict
 
-def create_notes_dict(lineage_notes):
+def create_notes_dict(lineages_notes):
     # add lineages and sub lineages notes into a dict keyed by lineage name
     href_dict = {}
     just_href = {}
-    lineage_notes = collections.defaultdict(list)
-    with open(lineage_notes,"r") as f:
+    lineages_notes_dict = collections.defaultdict(list)
+    with open(lineages_notes,"r") as f:
         for l in f:
             l = l.rstrip("\n")
             lineage,description = l.split("\t")
             if lineage != "lineage":
+                lineage = lineage.lstrip("*")
                 href_dict[lineage] = """<a href="{{ 'lineages/lineage_""" + lineage + """.html' | absolute_url }}">""" + lineage + """</a>"""
                 just_href[lineage] = """"{{ 'lineages/lineage_""" + lineage + """.html' | absolute_url }}"""
                 for i in range(len(lineage.split("."))):
                     parents = ".".join(lineage.split(".")[:i+1])
-                    lineage_notes[parents].append((lineage, description))
-    return lineage_notes, href_dict, just_href
+                    lineages_notes_dict[parents].append((lineage, description))
+    return lineages_notes_dict, href_dict, just_href
 
 def make_summary_dict(summary_file):
     # add lineages and sub lineages into a dict with verity's summary information about each lineage
@@ -68,65 +69,81 @@ def make_summary_dict(summary_file):
                     summary_dict[parents].append((lineage, tokens))
     return summary_dict
 
-def create_lineage_and_assignment_pages(outfile, website_dir,assignment_dir, summary_figures, lineage_dict, lineage_notes, summary_dict, href_dict, just_href):
+def create_lineage_and_assignment_pages(outfile, website_dir,assignment_dir, summary_figures, lineage_dict, lineage_notes, lineages_notes_file, summary_dict, href_dict, just_href):
     with open(outfile,"w") as fall:
         # write preamble to descriptions file
         descriptions_text = return_descriptions_text()
         fall.write(descriptions_text)
 
-        # populating the assignment directory with metadata in a directory tree
-        for lineage in sorted(lineage_dict):
-            inter_dirs = [assignment_dir]
-            for i in range(len(lineage.split("."))):
-                parent = ".".join(lineage.split(".")[:i+1])
-                inter_dirs.append(parent)
+        
+        with open(lineages_notes_file, "r") as f:
+            for l in f:
+                l = l.rstrip("\n")
+                lineage,notes = l.split("\t")
+                if lineage !="Lineage":
+                    dead = False
+                    if lineage.startswith("*"):
+                        lineage = lineage.lstrip("*")
+                        dead = True
 
-            parent_dir = "/".join(inter_dirs[:-1])
-            parent_directory = os.path.join(parent_dir)
-            if not os.path.exists(parent_directory):
-                os.mkdir(parent_directory)
-            
-            with open(os.path.join(parent_directory,f"{lineage}.metadata.csv"), "w") as fw:
-                fw.write("GISAID ID,name,country,travel history,sample date,epiweek,lineage,representative\n")
-                print(lineage)
-                for line in sorted(lineage_dict[lineage], key = lambda x : x[0]):
-                    fw.write(line[1] + '\n')
-            
-            # write the descriptions to descriptions.md for each lineage, with hyperlinks
-            descriptions = sorted(lineage_notes[lineage], key = lambda x : x[0])
-            lin_desc = descriptions[0][1]
-            fall.write("""<li>""" + href_dict[lineage] + """Lineage """ + lineage + f"""</a> <br>{lin_desc}<br></li>\n\t\t""")
+                    # populating the assignment directory with metadata in a directory tree
+                    inter_dirs = [assignment_dir]
+                    for i in range(len(lineage.split("."))):
+                        parent = ".".join(lineage.split(".")[:i+1])
+                        inter_dirs.append(parent)
 
-            # create lineage webpage
-            with open(os.path.join(web_dir, f"lineage_{lineage}.md"),"w") as fw:
-                fw.write(f"""---\nlayout: page\ntitle: Lineage {lineage}\n---\n\n\n\n""")
-                
-                # for anything but A or B, create a hyperlink to the parent lineage at the top of the webpage
-                if "." in lineage:
-                    parent_lineage = ".".join(lineage.split(".")[:-1])
-                    fw.write(f"""<p>\n<ul class="actions small">\n\t <a href=""" + just_href[parent_lineage] + """" class="button special fit">Go to parent lineage: """+parent_lineage+"""</a>\n</ul>\n</p>\n""")
-                
-                # get the lineage svg figures
-                fw.write(f"<h3> Lineage summaries</h3>\n\n")
-                os.system(f"cp {summary_figures}/{lineage}.svg {website_dir}/assets/images/{lineage}.svg")
-                fw.write(f"""<img src="../assets/images/{lineage}.svg" alt="{lineage} lineage summary figure" width="90%" height="700px" />\n\n\n""")
+                    parent_dir = "/".join(inter_dirs[:-1])
+                    parent_directory = os.path.join(parent_dir)
+                    if not os.path.exists(parent_directory):
+                        os.mkdir(parent_directory)
+                    
+                    with open(os.path.join(parent_directory,f"{lineage}.metadata.csv"), "w") as fw:
+                        fw.write("GISAID ID,name,country,travel history,sample date,epiweek,lineage,representative\n")
+                        print(lineage)
+                        for line in sorted(lineage_dict[lineage], key = lambda x : x[0]):
+                            fw.write(line[1] + '\n')
+                    
+                    # write the descriptions to descriptions.md for each lineage, with hyperlinks
+                    descriptions = sorted(lineage_notes[lineage], key = lambda x : x[0])
+                    print(descriptions)
+                    lin_desc = descriptions[0][1]
+                    if dead:
+                        fall.write("""<li>""" + href_dict[lineage] +  f"""<br><strong>Low support, has been reassigned to parent lineage.</strong> {lin_desc}<br></li>\n\t\t""")
+                    else:
+                        fall.write("""<li>""" + href_dict[lineage] +  f"""<br>{lin_desc}<br></li>\n\t\t""")
+                    # create lineage webpage
+                    with open(os.path.join(website_dir, "lineages", f"lineage_{lineage}.md"),"w") as fw:
+                        fw.write(f"""---\nlayout: page\ntitle: Lineage {lineage}\n---\n\n\n\n""")
+                        if dead:
+                            fw.write("<strong>Lineage reassigned to parent lineage due to low support</strong>")
+                        # for anything but A or B, create a hyperlink to the parent lineage at the top of the webpage
+                        if "." in lineage:
+                            parent_lineage = ".".join(lineage.split(".")[:-1])
+                            fw.write(f"""<p>\n<ul class="actions small">\n\t <a href=""" + just_href[parent_lineage] + """" class="button special fit">Go to parent lineage: """+parent_lineage+"""</a>\n</ul>\n</p>\n""")
+                            
+                        # get the lineage svg figures
+                        fw.write(f"<h3> Lineage summaries</h3>\n\n")
+                        source = f"{summary_figures}/{lineage}.svg"
+                        destination = f"{website_dir}/assets/images/{lineage}.svg"
+                        os.system(f"cp {source} {destination}")
+                        fw.write(f"""<img src="../assets/images/{lineage}.svg" alt="{lineage} lineage summary figure" width="90%" height="700px" />\n\n\n""")
 
-                # write lineage summary information to a md table. 
-                fw.write("""| Lineage name | Most common countries | Date range | Number of taxa |  Days since last sampling | Known Travel | Recall value |\n|:-----|:-----|:-------|-------:|-------:|:---------|--------:|\n""")
-                for line in sorted(summary_dict[lineage], key = lambda x : x[0]):
-                    items = line[1]
-                    href = href_dict[items[0]]
-                    table_items = " | ".join(items[1:])
-                    fw.write(f"| {href} | {table_items} |\n")
+                        # write lineage summary information to a md table. 
+                        fw.write("""| Lineage name | Most common countries | Date range | Number of taxa |  Days since last sampling | Known Travel | Recall value |\n|:-----|:-----|:-------|-------:|-------:|:---------|--------:|\n""")
+                        for line in sorted(summary_dict[lineage], key = lambda x : x[0]):
+                            items = line[1]
+                            href = href_dict[items[0]]
+                            table_items = " | ".join(items[1:])
+                            fw.write(f"| {href} | {table_items} |\n")
 
-                # write lineage descriptions to a md table. 
-                fw.write("\n<h3>Lineage descriptions</h3>\n\n")
-                fw.write(f"""| Lineage | Notes |\n|:-----|:-----|\n""")
-                for d in descriptions:
-                    lin = d[0]
-                    link_lin = href_dict[lin]
-                    fw.write(f"| {link_lin} | {d[1]} |\n")
-                fw.write("\n")
+                        # write lineage descriptions to a md table. 
+                        fw.write("\n<h3>Lineage descriptions</h3>\n\n")
+                        fw.write(f"""| Lineage | Notes |\n|:-----|:-----|\n""")
+                        for d in descriptions:
+                            lin = d[0]
+                            link_lin = href_dict[lin]
+                            fw.write(f"| {link_lin} | {d[1]} |\n")
+                        fw.write("\n")
 
         # finish off the main lineage descriptions webpage
         fall.write("</ul>\n</p>\n")
@@ -134,11 +151,11 @@ def create_lineage_and_assignment_pages(outfile, website_dir,assignment_dir, sum
 def update_pages():
     args = parse_args()
 
-    lineage_dict = create_lineage_dict(args.lineage_csv)
-    lineage_notes, href_dict, just_href = create_notes_dict(args.lineage_notes)
+    lineage_dict = create_lineage_dict(args.lineages_csv)
+    lineage_notes, href_dict, just_href = create_notes_dict(args.lineages_notes)
     summary_dict = make_summary_dict(args.summary_file)
     
-    create_lineage_and_assignment_pages(args.outfile,args.website_dir,args.assignment_dir, args.summary_figures, lineage_dict, lineage_notes, summary_dict, href_dict, just_href)
+    create_lineage_and_assignment_pages(args.outfile,args.website_dir,args.assignment_dir, args.summary_figures, lineage_dict, lineage_notes, args.lineages_notes, summary_dict, href_dict, just_href)
 
 if __name__ == '__main__':
 
